dc42332b098d9aa55d2d1525ea064dc098aa1322
unknown <synergy2411@outlook.com>
2020-02-05 13:08:37 +0530

    reactive form added

--- users-app/src/app/components/auth/register/register.component.ts
+++ users-app/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  username = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+    this.isExclamationAvailable
+  ]);
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      username: this.username,
+      password: this.password
+    })
+  }
+
+  isExclamationAvailable(control: FormControl) {
+    const hasExclamtion = control.value.indexOf("!") >= 0;
+    return hasExclamtion ? null : { needExclamtion: true }
+  }
+
+  onRegister() {
+    console.log(this.registerForm);
+  }
 
   ngOnInit() {
   }
